c99e5e2f51a8bc0814133f4ede1c78c0aad58c3d
Sara Guillen <saraguillencole@gmail.com>
2025-03-26 20:19:33 +0100

    Paginas del LandingPage

--- src/modulorum/landing/paginae/DomusPagina.vue
+++ src/modulorum/landing/paginae/DomusPagina.vue
@@ -1,5 +1,14 @@
 <template>
     
-    <h1>Home Page</h1>
+    <div class="center">
+        <h1 class="text-4xl sm:texte-5xl font-bold tracking-tighter">
+            Bienvenido a nuestro sitio web
+        </h1>
     
+
+    <p class="md:text-xl text-grey-500 mx-auto w-[600px]">
+        Donum mox recipies pro fidelitate erga fructus nostros
+    </p>
+</div>
+
 </template>
